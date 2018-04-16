cfe2c5fba3c52359febaf813bdb081b1f9db56a7
footoredo <footoredo@gmail.com>
2018-04-16 19:52:18 +0800

    Supporting oj

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d out -classpath "lib/antlr-4.7.1-complete.jar" @/dev/stdin
+find ./src -name *.java | javac -d bin -classpath "lib/antlr-4.7.1-complete.jar:lib/junit-4.12.jar" @/dev/stdin
