7360f70b68f96b1088bce9b0370664b49cc6e800
footoredo <footoredo@gmail.com>
2018-05-30 12:55:37 +0800

    asm s06e00 "LETS GO BOIS"

---
+++ optim.bash
@@ -0,0 +1,7 @@
+set -e
+cd "$(dirname "$0")"
+export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./lib/junit-4.12.jar:./bin cat.footoredo.mx.OJ.Tester"
+cat > program.m # save everything in stdin to program.txt
+$CCHK
+
+# nasm -felf64 program.asm && gcc program.o && ./a.out
